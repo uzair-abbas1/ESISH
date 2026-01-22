6b09d29abfa63086b95bf15ad6347fd47fea567a
Uzair <uzair.abbas600@gmail.com>
2026-01-22 17:43:55 +0200

    Basic Setup to display .tff custom characters. Added file to display char 'a' and 'A'

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from custom_keyboard import *
 
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# Path to your font file
+font_path = 'ESISHTest1.ttf'
+
+# Load the font
+prop = fm.FontProperties(fname=font_path)
+
+# Create a figure
+fig, ax = plt.subplots(figsize=(10, 4))
+
+# Display text using your custom font
+message = "Hello World!\nABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz"
+ax.text(0.5, 0.5, message, fontproperties=prop, fontsize=24,
+        ha='center', va='center')
+
+# Remove axes
+ax.axis('off')
+
+# Show the result
+plt.tight_layout()
+plt.show()
